92e39fb08bcb287cbd4021b25d807ce90110c0c5
eistonCloudole <eiston@thecloudole.com>
2020-10-12 23:59:51 -0400

    connect shopify to expressjs

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,23 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-
+const dotenv = require('dotenv').config()
 const cors = require('cors');
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+const axios = require('axios');
+
 app.use(cors());
 
 const { db } = require('./util/admin');
+
+const apiKey = '3184f521fc3473624d2142ae452aaec6';
+const apiSecret = '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';
+const scopes = 'read_products';
+const forwardingAddress = "https://cloudole-2f23d.web.app";
 
 const { signup, login, addUserDetails, getUser} = require('./handlers/user');
 
@@ -14,7 +26,74 @@ app.post('/login', login);
 app.get('/users', getUser);
 app.post('/users', FBAuth, addUserDetails);
 
+app.get('/shopify', (req, res) => {
+    const shop = req.query.shop;
+    if (shop) {
+      const state = nonce();
+      const redirectUri = forwardingAddress + '/shopify/callback';
+      const installUrl = 'https://' + shop +
+        '/admin/oauth/authorize?client_id=' + apiKey +
+        '&scope=' + scopes +
+        '&state=' + state +
+        '&redirect_uri=' + redirectUri;
+  
+      res.cookie('state', state);
+      return res.redirect(installUrl);
+    } else {
+      return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
+});
+app.get('/shopify/callback', (req, res) => {
+    const { shop, hmac, code, state } = req.query;
+    // const stateCookie = cookie.parse(req.headers.cookie).state;
+  
+    // if (state !== stateCookie) {
+    //   return res.status(403).send('Request origin cannot be verified');
+    // }
+  
+    if (shop && hmac && code) {
+    //   // DONE: Validate request is from Shopify
+    //   const map = Object.assign({}, req.query);
+    //   delete map['signature'];
+    //   delete map['hmac'];
+    //   const message = querystring.stringify(map);
+    //   const providedHmac = Buffer.from(hmac, 'utf-8');
+    //   const generatedHash = Buffer.from(
+    //     crypto
+    //       .createHmac('sha256', apiSecret)
+    //       .update(message)
+    //       .digest('hex'),
+    //       'utf-8'
+    //     );
+    //   let hashEquals = false;
+  
+    //   try {
+    //     hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    //   } catch (e) {
+    //     hashEquals = false;
+    //   }
+  
+    //   if (!hashEquals) {
+    //     return res.status(400).send('HMAC validation failed');
+    //   }
+      // DONE: Exchange temporary code for a permanent access token
+      const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+      const accessTokenPayload = {
+        client_id: apiKey,
+        client_secret: apiSecret,
+        code,
+      };
+      // return res.status(200).send(accessTokenPayload)
+
+      axios.post(accessTokenRequestUrl, accessTokenPayload)
+      .then((response) => {
+        return res.status(200).send('accessToken: ' + response.accessToken)
+      })
+      .catch((error) => {
+        return res.status(error.statusCode).send(error.error.error_description);
+      });
+    } else {
+        res.status(400).send('Required parameters missing');
+    }
+});
 exports.api = functions.https.onRequest(app)
-
-
-
