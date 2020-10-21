1d5448cd48efd35b079cd42351027c5abfc6df2a
eiston <eiston.wei@gmail.com>
2020-10-21 15:09:50 -0400

    sent token and shop

--- functions/handlers/shopify.js
+++ functions/handlers/shopify.js
@@ -4,6 +4,7 @@ const apiKey = '3184f521fc3473624d2142ae452aaec6';
 const apiSecret = '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';
 const scopes = 'read_products';
 const forwardingAddress = "https://us-central1-cloudole-2f23d.cloudfunctions.net/api";
+const axios = require('axios');
 
 exports.shopifyLogin = (req, res) => {
     const shop = req.query.shop;
@@ -36,8 +37,11 @@ exports.shopifyRedirect = (req, res) => {
       axios.post(accessTokenRequestUrl, accessTokenPayload)
       .then((response) => {
         console.log(response.data)
-        req.app.set('access_token', response.data.access_token)
-        return res.redirect('/api/token')
+        // req.app.set('access_token', response.data.access_token)
+        let data_copy = JSON.parse(JSON.stringify(response.data))
+        // return res.redirect('/api/token')
+        data_copy['shop'] = shop
+        return res.status(200).send(data_copy)
       })
       .catch((error) => {
         return res.status(error.statusCode).send(error);
