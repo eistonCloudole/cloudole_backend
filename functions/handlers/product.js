5d8718a0135a37148a602f6b9dd897c61df496b2
eiston <eiston.wei@gmail.com>
2020-12-26 16:49:18 -0500

    addShoppingCart

--- functions/handlers/product.js
+++ functions/handlers/product.js
@@ -1,9 +1,10 @@
-const {shopifyProductList, modifyInventory} = require("./shopifyApi")
+const { shopifyProductList, modifyInventory } = require("./shopifyApi")
+const { admin, db } = require("../util/admin")
 
 exports.productList = (req, res) => {
     console.log(req.header('shopifyToken'))
     const apiInfo = {
-        shopifyShopName:req.header('shopifyShopName'),
+        shopifyShopName: req.header('shopifyShopName'),
         shopifyToken: req.header('shopifyToken'),
     }
     // const apiInfo = {
@@ -11,31 +12,43 @@ exports.productList = (req, res) => {
     //     shopifyToken: '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'
     // }
     return shopifyProductList(apiInfo.shopifyShopName, apiInfo.shopifyToken)
-    .then((products) => {
-        //console.log(products)
-        return res.status(200).json(products)
-    })
-    .catch((error)=> {
-        console.log(error.response)
-    })
+        .then((products) => {
+            //console.log(products)
+            return res.status(200).json(products)
+        })
+        .catch((error) => {
+            console.log(error.response)
+        })
 }
-
-exports.modifyInventory = (req, res) => {
+exports.addShoppingCart = async (req, res) => {
+    const cart = db.collection('shoppingCarts').doc(req.body.shopifyShopName);
+    const quantity_key = `${req.body.location_id}.${req.body.inventory_item_id}.quantity`
+    const price_key = `${req.body.location_id}.${req.body.inventory_item_id}.price`
+    try {
+        await cart.update({
+            [quantity_key]: admin.firestore.FieldValue.increment(50),
+            [price_key]: req.body.price
+        });
+        return res.status(200)
+    } catch (error) {
+        return res.status(400).json(error)
+    }
+}
+exports.modifyInventory = async (req, res) => {
     const apiInfo = {
-        shopifyShopName:req.body.shopifyShopName,
+        shopifyShopName: req.body.shopifyShopName,
         shopifyToken: req.body.shopifyToken,
     }
     const body = {
-        location_id: req.body.location_id,
+        location_id : req.body.location_id,
         inventory_item_id: req.body.inventory_item_id,
-        available_adjustment: req.body.available_adjustment
+        available_adjustment: req.body.available_adjustment,
     }
-    return modifyInventory(apiInfo.shopifyShopName, apiInfo.shopifyToken, body)
-    .then((products) => {
-        console.log(products)
+
+    try {
+        const products = modifyInventory(apiInfo.shopifyShopName, apiInfo.shopifyToken, body);
         return res.status(200).json(products)
-    })
-    .catch((error)=> {
-        console.log(error.response)
-    })
+    } catch (error) {
+        return res.status(400).json(error)
+    }
 }
